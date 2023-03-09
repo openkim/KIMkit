72082a31dcdee99234f50e7c007e0c30b0775fe3
Brendon Waters <noreply+waters20@llnl.gov>
2023-03-09 14:35:52 -0600

    MAINT: remove unused install options
    Install into "environment variable" collection
    by default to have system accessable collection
    that can be managed in one environment file

--- kimapi.py
+++ kimapi.py
@@ -87,129 +87,20 @@ def make_object(obj, approved=True):
             check_call(["echo", "%r" % obj], stdout=log, stderr=log)
 
             try:
-                if leader in ["md", "mo", "sm"]:
-                    if approved:
-                        check_call(
-                            [
-                                "kim-api-collections-management",
-                                "install",
-                                "user",
-                                obj.path,
-                            ],
-                            stdout=log,
-                            stderr=log,
-                        ),
-                    else:
-                        check_call(
-                            [
-                                "kim-api-collections-management",
-                                "install",
-                                "environment",
-                                obj.path,
-                            ],
-                            stdout=log,
-                            stderr=log,
-                        ),
-
-                elif leader in ["td", "te", "vc"]:
-
-                    # First, check for a makefile
-                    possible_makefile_names = ["GNUmakefile", "makefile", "Makefile"]
-
-                    found_makefile = False
-                    for makefile_name in possible_makefile_names:
-                        if os.path.isfile(os.path.join(obj.path, makefile_name)):
-                            found_makefile = True
-                            break
-
-                    if found_makefile:
-                        check_call(["make"], stdout=log, stderr=log)
-
-                    else:
-                        # Try to build with cmake in test directory (since
-                        # nothing from runners gets installed anywhere
-                        # specifically)
-                        check_call(
-                            [
-                                "cmake",
-                                obj.path,
-                                "-DCMAKE_BUILD_TYPE=" + cf.CMAKE_BUILD_TYPE,
-                            ],
-                            cwd=obj.path,
-                            stdout=log,
-                            stderr=log,
-                        )
-                        check_call(["make"], cwd=obj.path, stdout=log, stderr=log)
+                check_call(
+                    [
+                        "kim-api-collections-management",
+                        "install",
+                        "environment",
+                        obj.path,
+                    ],
+                    stdout=log,
+                    stderr=log,
+                ),
 
                 check_call(["touch", "built-by-%s" % cf.UUID], stdout=log, stderr=log)
 
             except CalledProcessError:
-                driver = obj.driver
-                if (not approved) and driver:
-                    # Here, we perform an extra check to see if the compilation
-                    # of this item failed simply because it's a pending item
-                    # with a pending driver and the driver wasn't found during
-                    # the build attempt. This doesn't apply to Models because
-                    # we store pending drivers in the environment variable
-                    # collection (just as we do for pending Models). Since VCs
-                    # cannot have drivers, this means we only need to proceed
-                    # if this item is a pending Test that uses a driver.
-                    if leader != "te":
-                        pass
-                    else:
-                        # If we are attempting to build a Test whose driver is only
-                        # present in the local 'pending' directory of the Director, create
-                        # a temporary symlink where the driver would sit in the
-                        # LOCAL_REPOSITORY_PATH to where it sits in the 'pending' directory
-                        src = []
-                        symlink_path = []
-                        src.append(
-                            os.path.join(
-                                cf.LOCAL_REPOSITORY_PATH, "pending", "td", driver
-                            )
-                        )
-                        symlink_path.append(
-                            os.path.join(cf.LOCAL_REPOSITORY_PATH, "td", driver)
-                        )
 
-                        try:
-                            for s, l in zip(src, symlink_path):
-                                os.symlink(s, l)
-                        except CalledProcessError:
-                            logger.exception(
-                                "Failed to create driver symlink for build of pending item %r"
-                                % obj
-                            )
-                            logger.exception(
-                                "Could not build %r, check %s" % (obj, MAKE_LOG)
-                            )
-                            raise cf.KIMBuildError(
-                                "Could not build %r, check %s" % (obj, MAKE_LOG)
-                            )
-
-                        # Now try to build one more time before giving up
-                        try:
-                            # TODO: Need to check for CMakeLists.txt and act accordingly
-                            #        (can we just do a `make install`?)
-                            check_call(["make"], stdout=log, stderr=log)
-                            check_call(
-                                ["touch", "built-by-%s" % cf.UUID],
-                                stdout=log,
-                                stderr=log,
-                            )
-                        except CalledProcessError:
-                            logger.exception(
-                                "Could not build %r, check %s" % (obj, MAKE_LOG)
-                            )
-                            raise cf.KIMBuildError(
-                                "Could not build %r, check %s" % (obj, MAKE_LOG)
-                            )
-
-                        # Delete symlinks
-                        for l in symlink_path:
-                            os.remove(l)
-                else:
-                    logger.exception("Could not build %r, check %s" % (obj, MAKE_LOG))
-                    raise cf.KIMBuildError(
-                        "Could not build %r, check %s" % (obj, MAKE_LOG)
-                    )
+                logger.exception("Could not build %r, check %s" % (obj, MAKE_LOG))
+                raise cf.KIMBuildError("Could not build %r, check %s" % (obj, MAKE_LOG))
