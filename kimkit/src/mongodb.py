2bc06683be2b288411280129c119f6cc427c465a
Brendon Waters <bwaters@umn.edu>
2024-08-07 12:52:51 -0500

    BUG: revert llnl specific mongo config options

--- kimkit/src/mongodb.py
+++ kimkit/src/mongodb.py
@@ -23,17 +23,7 @@ from .. import users
 
 logger = logging.getLogger("KIMkit")
 
-user = cf.MONGODB_USERNAME
-password = cf.MONGODB_PASSWORD
-host = cf.MONGODB_HOSTNAME
-port = cf.MONGODB_PORT
-db_name = cf.MONGODB_DATABASE
-args = "ssl=true&tlsAllowInvalidCertificates=true"
-
-db_uri = "mongodb://%s:%s@%s:%s/%s?%s" % (user, password, host, port, db_name, args)
-
-
-client = pymongo.MongoClient(host=db_uri)
+client = pymongo.MongoClient(host=cf.MONGODB_HOSTNAME)
 db = client[cf.MONGODB_DATABASE]
 
 BADKEYS = {"kimspec", "profiling", "inserted_on", "latest"}
